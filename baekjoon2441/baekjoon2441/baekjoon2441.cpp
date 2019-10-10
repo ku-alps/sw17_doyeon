3137217c41a672644130b4d3d8513ed4f26fecca
kimdoyeon <98ehdusdl@naver.com>
2019-10-11 01:18:16 +0900

    별찍기 탈출

---
+++ baekjoon2441/baekjoon2441/baekjoon2441.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < n - i; j++) cout << " ";
+		for (int j = n - i; j < n; j++) cout << "*";
+		cout << endl;
+	}
+	return 0;
+}
